701c36f3d01b94790af6fc4bf1171769056c626c
Xu, Yunlong | Long | GHRD <yunlong.xu@mail.rakuten.com>
2014-10-31 15:23:40 +0900

    add server api for update location

--- client/components/googlemap/googlemap.service.js
+++ client/components/googlemap/googlemap.service.js
@@ -3,5 +3,5 @@
 angular.module('travelIntelligenceApp')
   .service('googlemap', function () {
     // AngularJS will instantiate a singleton by calling "new" on this function
-	alert('googlemap');
+	// alert('googlemap');
   });
--- server/api/chat/chat.socket.js
+++ server/api/chat/chat.socket.js
@@ -5,4 +5,12 @@ exports.register = function(socket) {
     console.info('[%s] %s', socket.address, JSON.stringify(data, null, 2));
 		socket.broadcast.emit('broadcast', data);
   });
+  
+  /*
+  data = {id, type, location}
+  */
+  socket.on('move', function (data) {
+	console.info('[%s]', JSON.stringify(data, null, 2));
+	socket.broadcast.emit('move', data);
+  });
 }

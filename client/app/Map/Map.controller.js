3b12bc0eb1d9c0a9518af3045120d9d28bb6285c
Xu, Yunlong | Long | GHRD <yunlong.xu@mail.rakuten.com>
2014-11-07 09:25:31 +0900

    refactor for the first time

--- client/app/Map/Map.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('travelIntelligenceApp')
-  .controller('MapCtrl', function ($scope, googlemap) {
-    $scope.message = 'Hello';
-  });
--- client/app/app.js
+++ client/app/app.js
@@ -4,8 +4,17 @@ angular.module('travelIntelligenceApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ui.router'
+  'ui.router',
+  'ui.bootstrap',
+	'google-maps'.ns()
 ])
+	.config(['GoogleMapApiProvider'.ns(), function (GoogleMapApi) {
+	    GoogleMapApi.configure({
+	        key: 'AIzaSyDP9sT9ttOPhK8xhoqV5L6Uqg776GiVKjQ',
+	        v: '3.17',
+	        libraries: 'weather,geometry,visualization'
+	    });
+	}])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider
       .otherwise('/');
